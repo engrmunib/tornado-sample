b8d564893a80346e5d59cc7e772da41c6bec1fb9
Engr Munib <engr.munib07@gmail.com>
2020-10-13 17:35:54 +0500

    version print

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #!/opt/chi/python/bin/python
-
+import os
 from dotenv import load_dotenv
 
 
 def main():
     load_dotenv(verbose=True)
+    print(f"Version: {os.environ.get('VERSION')}")
     print('hello world!')
 
 
